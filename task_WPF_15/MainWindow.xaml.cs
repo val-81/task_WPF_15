3e09b5910612874b27607970d6793682bbf5f777
Валентин Ковальчук <val-81@mail.ru>
2022-01-26 01:48:31 +0300

    Добавьте файлы проекта.

---
+++ task_WPF_15/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task_WPF_15
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            text.ClearValue(FlowDocumentScrollViewer.DocumentProperty);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("Hymn.xaml", FileMode.Create))
+            {
+                XamlWriter.Save(text.Document, fs);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("Hymn.xaml", FileMode.Open))
+            {
+                text.Document = XamlReader.Load(fs) as FlowDocument;
+            }
+        }
+
+    }
+}
